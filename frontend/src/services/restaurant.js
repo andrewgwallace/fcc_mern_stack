3ce48b4471a431ff2040d94f5522733a81470050
Andrew Wallace <andrew.g.wallace@gmail.com>
2021-05-13 18:34:07 -0400

    use realm connection in frontend app instead of express and node

--- frontend/src/services/restaurant.js
+++ frontend/src/services/restaurant.js
@@ -2,27 +2,28 @@ import http from "../helpers/http.js";
 
 class RestaurantDataService {
   getAll(page = 0) {
-    return http.get(`?page=${page}`);
+    return http.get(`restaurants?page=${page}`);
   }
 
   get(id) {
-    return http.get(`/id/${id}`);
+    // return http.get(`/id/${id}`); // works for node/express only.
+    return http.get(`/restaurant?id=${id}`); // required for Realm. Realm does not support path parameters, only query ('?') parameters.
   }
 
   find(query, by = "name", page = 0) {
-    return http.get(`?${by}=${query}&page=${page}`);
+    return http.get(`restaurants?${by}=${query}&page=${page}`);
   } 
 
   createReview(data) {
-    return http.post("/review", data);
+    return http.post("/review-new", data);
   }
 
   updateReview(data) {
-    return http.put("/review", data);
+    return http.put("/review-edit", data);
   }
 
   deleteReview(id, userId) {
-    return http.delete(`/review?id=${id}`, {data:{user_id: userId}});
+    return http.delete(`/review-delete?id=${id}`, {data:{user_id: userId}});
   }
 
   getCuisines(id) {
